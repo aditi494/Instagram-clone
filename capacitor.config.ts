9f4619fe771d41492bec3e78bf2ec21d6ea61a68
aditi494 <70092640+aditi494@users.noreply.github.com>
2021-07-31 22:23:54 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.demoapp.instaclone',
+  appName: 'instaClone',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
